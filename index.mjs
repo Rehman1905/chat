d772e501340122668fb195905011474b8a56acb9
Rehman1905 <ll.mmdoff.ll@gmail.com>
2024-03-10 12:12:11 +0400

    51

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
-import db from '/firebase.js';
-import { ref, set, get, push ,onValue} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js'
+import db from './firebase.mjs';
+import { ref, set, get, push} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js'
 const section1=document.querySelector('#section1')
 const section2=document.querySelector('#section2')
 const ad=document.querySelector('#ad')
