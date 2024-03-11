f661b51dd4fc58b0fb2ceeaafbd1b219e0451ad9
Rehman1905 <ll.mmdoff.ll@gmail.com>
2024-03-11 16:03:51 +0400

    o

--- index.mjs
+++ index.mjs
@@ -26,15 +26,22 @@ document.querySelector('#btn').addEventListener('click',async function(e){
         if((await get(ref(db,`user/${use.key}`))).exists() && (adSon==use.username)&&(paswod==use.password)){
             section1.style.display='none'
             section2.style.display='block'
-            h1.innerHTML=`${ad.value} WELCOME`
+            h1.innerHTML=`${ad.value} Welcome`
             ad.value=''
             password.value=''
 
         }else{
             if(adSon==use.username&&paswod!=use.password){
-                alert('Password səhvdi')
+                password.style.border='2px solid red'
+                password.addEventListener('focus', function() {
+                    password.style.borderColor = 'red'; 
+                });
+                password.addEventListener('blur', function() {
+                    password.style.borderColor = ''; 
+                });
             }else{
-                alert('Qeydiyyatdan keç')
+                ad.style.border='2px solid red'
+                password.style.border='2px solid red'
             }
             password.value=''
             section1.style.display='flex'
@@ -106,21 +113,20 @@ document.querySelector('#send').addEventListener('click',function(e){
         alert('BOSH')
     }
 })
+const messageDiv=document.querySelector('#chat')
+
 function ge(){
     get(ref(db,'chat')).then(e=>{
         const user=JSON.parse(localStorage.getItem('user'))
         const chat=e.val()
-        const messageDiv=document.querySelector('#chat')
         messageDiv.classList.add('messageDiv')
         messageDiv.innerHTML=''
         for(let i in chat){
             const message=chat[i]
             const newDiv=document.createElement('div')
             newDiv.classList.add('messageDiv')
-            newDiv.innerHTML=`${message.user.username}:${message.text}`
+            newDiv.innerHTML=`${message.user.username}: ${message.text}`
               messageDiv.append(newDiv) 
-              console.log(user.key)
-              console.log(message.user.key)
             if (user.key === message.user.key) {
                 newDiv.classList.add('my-message')
             } else {
@@ -128,5 +134,8 @@ function ge(){
             }
         }
     })
+    messageDiv.scrollTop = messageDiv.scrollHeight;
 }
-setInterval(ge,1000)
+
+
+setInterval(ge,1)
